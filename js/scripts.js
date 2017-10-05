73dcbb2a8028a7b697a48acf5e665b21e41a7d42
Lieghti <lieghti@gmail.com>
2017-10-05 13:13:42 -0700

    add all of the calculators for the different functions, commented out.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+/* This is for ADDING
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,3 +8,41 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+*/
+
+/* This is for SUBTRACTING
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+*/
+
+/* This is for MULTIPLYING
+
+var product = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = product(number1, number2);
+alert(result);
+*/
+
+
+/* This is for DIVIDING
+
+var quotient = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = quotient(number1, number2);
+alert(result);
+*/
